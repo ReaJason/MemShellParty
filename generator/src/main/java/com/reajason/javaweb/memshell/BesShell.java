e6fb68b0e784148b1a7e84bdd6f9ac5b6b7f85c2
ReaJason <reajason1225@gmail.com>
2025-01-10 01:07:34 +0800

    feat: suport bes agent shell

--- generator/src/main/java/com/reajason/javaweb/memshell/BesShell.java
+++ generator/src/main/java/com/reajason/javaweb/memshell/BesShell.java
@@ -3,12 +3,13 @@ package com.reajason.javaweb.memshell;
 import com.reajason.javaweb.memshell.bes.behinder.BehinderValve;
 import com.reajason.javaweb.memshell.bes.command.CommandValve;
 import com.reajason.javaweb.memshell.bes.godzilla.GodzillaValve;
-import com.reajason.javaweb.memshell.bes.injector.BesFilterInjector;
-import com.reajason.javaweb.memshell.bes.injector.BesListenerInjector;
-import com.reajason.javaweb.memshell.bes.injector.BesValveInjector;
+import com.reajason.javaweb.memshell.bes.injector.*;
 import com.reajason.javaweb.memshell.shelltool.behinder.BehinderFilter;
+import com.reajason.javaweb.memshell.shelltool.behinder.BehinderFilterChainAdvisor;
 import com.reajason.javaweb.memshell.shelltool.command.CommandFilter;
+import com.reajason.javaweb.memshell.shelltool.command.CommandFilterChainAdvisor;
 import com.reajason.javaweb.memshell.shelltool.godzilla.GodzillaFilter;
+import com.reajason.javaweb.memshell.shelltool.godzilla.GodzillaFilterChainAdvisor;
 import com.reajason.javaweb.memshell.tomcat.behinder.BehinderListener;
 import com.reajason.javaweb.memshell.tomcat.command.CommandListener;
 import com.reajason.javaweb.memshell.tomcat.godzilla.GodzillaListener;
@@ -29,7 +30,9 @@ public class BesShell extends AbstractShell {
         return Map.of(
                 FILTER, Pair.of(CommandFilter.class, BesFilterInjector.class),
                 LISTENER, Pair.of(CommandListener.class, BesListenerInjector.class),
-                VALVE, Pair.of(CommandValve.class, BesValveInjector.class)
+                VALVE, Pair.of(CommandValve.class, BesValveInjector.class),
+                AGENT_FILTER_CHAIN, Pair.of(CommandFilterChainAdvisor.class, BesFilterChainAgentInjector.class),
+                AGENT_CONTEXT_VALVE, Pair.of(CommandFilterChainAdvisor.class, BesContextValveAgentInjector.class)
         );
     }
 
@@ -38,7 +41,9 @@ public class BesShell extends AbstractShell {
         return Map.of(
                 FILTER, Pair.of(GodzillaFilter.class, BesFilterInjector.class),
                 LISTENER, Pair.of(GodzillaListener.class, BesListenerInjector.class),
-                VALVE, Pair.of(GodzillaValve.class, BesValveInjector.class)
+                VALVE, Pair.of(GodzillaValve.class, BesValveInjector.class),
+                AGENT_FILTER_CHAIN, Pair.of(GodzillaFilterChainAdvisor.class, BesFilterChainAgentInjector.class),
+                AGENT_CONTEXT_VALVE, Pair.of(GodzillaFilterChainAdvisor.class, BesContextValveAgentInjector.class)
         );
     }
 
@@ -47,7 +52,9 @@ public class BesShell extends AbstractShell {
         return Map.of(
                 FILTER, Pair.of(BehinderFilter.class, BesFilterInjector.class),
                 LISTENER, Pair.of(BehinderListener.class, BesListenerInjector.class),
-                VALVE, Pair.of(BehinderValve.class, BesValveInjector.class)
+                VALVE, Pair.of(BehinderValve.class, BesValveInjector.class),
+                AGENT_FILTER_CHAIN, Pair.of(BehinderFilterChainAdvisor.class, BesFilterChainAgentInjector.class),
+                AGENT_CONTEXT_VALVE, Pair.of(BehinderFilterChainAdvisor.class, BesContextValveAgentInjector.class)
         );
     }
 }

19c40e0e2d6bf2ea8d00248ca68f668226b66a78
ReaJason <reajason1225@gmail.com>
2025-02-22 17:21:27 +0800

    refactor: rm useless code

--- deserialize/src/main/java/com/reajason/javaweb/deserialize/TemplateUtils.java
+++ deserialize/src/main/java/com/reajason/javaweb/deserialize/TemplateUtils.java
@@ -13,27 +13,11 @@ import net.bytebuddy.dynamic.DynamicType;
  * @since 2024/12/9
  */
 public class TemplateUtils {
-    public static final String ANN_INV_HANDLER_CLASS = "sun.reflect.annotation.AnnotationInvocationHandler";
-    public static Class TPL_CLASS = TemplatesImpl.class;
-    public static Class ABST_TRANSLET = AbstractTranslet.class;
-    public static Class TRANS_FACTORY = TransformerFactoryImpl.class;
-
-    static {
-        try {
-            // 兼容不同 JDK 版本
-            if (Boolean.parseBoolean(System.getProperty("properXalan", "false"))) {
-                TPL_CLASS = Class.forName("org.apache.xalan.xsltc.trax.TemplatesImpl");
-                ABST_TRANSLET = Class.forName("org.apache.xalan.xsltc.runtime.AbstractTranslet");
-                TRANS_FACTORY = Class.forName("org.apache.xalan.xsltc.trax.TransformerFactoryImpl");
-            }
-        } catch (Exception ignored) {
-        }
-    }
 
     @SneakyThrows
     public static TemplatesImpl createTemplatesImpl(byte[] bytes) {
         TemplatesImpl templates = new TemplatesImpl();
-        byte[] fooBytes = new byte[0];
+        byte[] fooBytes;
         try (DynamicType.Unloaded<Object> make = new ByteBuddy()
                 .subclass(Object.class).name("foo")
                 .make()) {

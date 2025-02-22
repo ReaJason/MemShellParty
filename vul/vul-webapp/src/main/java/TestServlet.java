d64a8b9e6bbcc2e9b70517bce7462ecf029bf696
ReaJason <reajason1225@gmail.com>
2025-02-22 17:38:09 +0800

    test: rm log

--- vul/vul-webapp/src/main/java/TestServlet.java
+++ vul/vul-webapp/src/main/java/TestServlet.java
@@ -13,11 +13,9 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(req.getClass().getName());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(req.getClass().getName());
     }
 }

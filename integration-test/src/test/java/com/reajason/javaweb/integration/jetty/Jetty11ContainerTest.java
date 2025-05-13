ca3c8c1e3c6d1d6941636f8926870db4ea0f3557
ReaJason <reajason1225@gmail.com>
2025-05-13 18:24:02 +0800

    test: add jetty ee8 ~ ee10 cases

--- integration-test/src/test/java/com/reajason/javaweb/integration/jetty/Jetty11ContainerTest.java
+++ integration-test/src/test/java/com/reajason/javaweb/integration/jetty/Jetty11ContainerTest.java
@@ -17,6 +17,7 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.apache.commons.lang3.tuple.Triple;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -33,7 +34,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @Slf4j
 @Testcontainers
 public class Jetty11ContainerTest {
-    public static final String imageName = "jetty:11-jre11";
+    public static final String imageName = "jetty:11.0-jre17";
     static Network network = Network.newNetwork();
     @Container
     public final static GenericContainer<?> python = new GenericContainer<>(new ImageFromDockerfile()
@@ -52,12 +53,19 @@ public class Jetty11ContainerTest {
     static Stream<Arguments> casesProvider() {
         Server server = Server.Jetty;
         List<String> supportedShellTypes = List.of(
-                ShellType.JAKARTA_SERVLET, ShellType.JAKARTA_FILTER, ShellType.JAKARTA_LISTENER,
-                ShellType.JETTY_AGENT_HANDLER,ShellType.JETTY_AGENT_HANDLER_ASM
+                ShellType.JAKARTA_SERVLET,
+                ShellType.JAKARTA_FILTER,
+                ShellType.JAKARTA_LISTENER,
+                ShellType.JETTY_AGENT_HANDLER,
+                ShellType.JETTY_AGENT_HANDLER_ASM
         );
-        List<Packers> testPackers = List.of(Packers.JSP, Packers.JSPX, Packers.JavaDeserialize);
+        List<Packers> testPackers = List.of(Packers.JSP);
+        List<Triple<String, ShellTool, Packers>> unSupportedCases = List.of(
+                Triple.of(ShellType.JETTY_AGENT_HANDLER, ShellTool.Godzilla, Packers.AgentJar),  // jdk module
+                Triple.of(ShellType.JETTY_AGENT_HANDLER, ShellTool.Behinder, Packers.AgentJar) // jdk module
+        );
         return TestCasesProvider.getTestCases(imageName, server, supportedShellTypes, testPackers,
-                null, List.of(ShellTool.AntSword) // AntSword not supported Jakarta
+                unSupportedCases, List.of(ShellTool.AntSword)
         );
     }
 
@@ -70,6 +78,6 @@ public class Jetty11ContainerTest {
     @ParameterizedTest(name = "{0}|{1}{2}|{3}")
     @MethodSource("casesProvider")
     void test(String imageName, String shellType, ShellTool shellTool, Packers packer) {
-        testShellInjectAssertOk(getUrl(container), Server.Jetty, shellType, shellTool, Opcodes.V11, packer, container, python);
+        testShellInjectAssertOk(getUrl(container), Server.Jetty, shellType, shellTool, Opcodes.V17, packer, container, python);
     }
 }
---
+++ vul/vul-webapp-jakarta/src/main/java/jakarta/Base64ClassLoaderServlet.java
@@ -0,0 +1,54 @@
+package jakarta;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+/**
+ * @author ReaJason
+ * @since 2024/12/24
+ */
+public class Base64ClassLoaderServlet extends ClassLoader implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        String data = req.getParameter("data");
+        try {
+            byte[] bytes = decodeBase64(data);
+            defineClass(null, bytes, 0, bytes.length).newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static byte[] decodeBase64(String base64Str) throws Exception {
+        try {
+            Class<?> decoderClass = Class.forName("sun.misc.BASE64Decoder");
+            return (byte[]) decoderClass.getMethod("decodeBuffer", String.class).invoke(decoderClass.newInstance(), base64Str);
+        } catch (Exception var4) {
+            Class<?> decoderClass = Class.forName("java.util.Base64");
+            Object decoder = decoderClass.getMethod("getDecoder").invoke((Object) null);
+            return (byte[]) decoder.getClass().getMethod("decode", String.class).invoke(decoder, base64Str);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

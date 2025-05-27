f9c1ac9535859c8b9a21dd9778792e099ec64413
ReaJason <reajason1225@gmail.com>
2025-05-27 20:45:58 +0800

    refactor: simplify shell generator

--- generator/src/main/java/com/reajason/javaweb/memshell/generator/BehinderGenerator.java
+++ generator/src/main/java/com/reajason/javaweb/memshell/generator/BehinderGenerator.java
@@ -1,15 +1,9 @@
 package com.reajason.javaweb.memshell.generator;
 
-import com.reajason.javaweb.ClassBytesShrink;
-import com.reajason.javaweb.buddy.LogRemoveMethodVisitor;
-import com.reajason.javaweb.buddy.ServletRenameVisitorWrapper;
-import com.reajason.javaweb.buddy.TargetJreVersionVisitorWrapper;
 import com.reajason.javaweb.memshell.config.BehinderConfig;
 import com.reajason.javaweb.memshell.config.ShellConfig;
 import com.reajason.javaweb.memshell.utils.DigestUtils;
-import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
-import org.apache.commons.lang3.StringUtils;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
@@ -17,46 +11,15 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
  * @author ReaJason
  * @since 2024/12/21
  */
-public class BehinderGenerator {
-    private final ShellConfig shellConfig;
-    private final BehinderConfig behinderConfig;
-
-    public BehinderGenerator(ShellConfig shellConfig, BehinderConfig behinderConfig) {
-        this.shellConfig = shellConfig;
-        this.behinderConfig = behinderConfig;
+public class BehinderGenerator extends ByteBuddyShellGenerator<BehinderConfig> {
+    public BehinderGenerator(ShellConfig shellConfig, BehinderConfig shellToolConfig) {
+        super(shellConfig, shellToolConfig);
     }
 
-    public DynamicType.Builder<?> getBuilder() {
-        if (behinderConfig.getShellClass() == null) {
-            throw new IllegalArgumentException("behinderConfig.getClazz() == null");
-        }
-        if (StringUtils.isBlank(behinderConfig.getPass())) {
-            throw new IllegalArgumentException("behinderConfig.getPass().isBlank()");
-        }
-        String md5Key = DigestUtils.md5Hex(behinderConfig.getPass()).substring(0, 16);
-
-        DynamicType.Builder<?> builder = new ByteBuddy()
-                .redefine(behinderConfig.getShellClass())
-                .name(behinderConfig.getShellClassName())
-                .visit(new TargetJreVersionVisitorWrapper(shellConfig.getTargetJreVersion()));
-
-        if (shellConfig.isJakarta()) {
-            builder = builder.visit(ServletRenameVisitorWrapper.INSTANCE);
-        }
-
-        if (shellConfig.isDebugOff()) {
-            builder = LogRemoveMethodVisitor.extend(builder);
-        }
-
+    public DynamicType.Builder<?> build(DynamicType.Builder<?> builder) {
+        String md5Key = DigestUtils.md5Hex(shellToolConfig.getPass()).substring(0, 16);
         return builder.field(named("pass")).value(md5Key)
-                .field(named("headerName")).value(behinderConfig.getHeaderName())
-                .field(named("headerValue")).value(behinderConfig.getHeaderValue());
-    }
-
-    public byte[] getBytes() {
-        DynamicType.Builder<?> builder = getBuilder();
-        try (DynamicType.Unloaded<?> make = builder.make()) {
-            return ClassBytesShrink.shrink(make.getBytes(), shellConfig.isShrink());
-        }
+                .field(named("headerName")).value(shellToolConfig.getHeaderName())
+                .field(named("headerValue")).value(shellToolConfig.getHeaderValue());
     }
 }
---
+++ generator/src/main/java/com/reajason/javaweb/memshell/generator/ShellGenerator.java
@@ -0,0 +1,10 @@
+package com.reajason.javaweb.memshell.generator;
+
+/**
+ * @author ReaJason
+ * @since 2025/5/27
+ */
+public interface ShellGenerator {
+
+    byte[] getBytes();
+}

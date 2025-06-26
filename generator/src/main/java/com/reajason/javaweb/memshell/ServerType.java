7144d1a6d3943be1e03a28a2115ce8fcbc2ae6ca
ReaJason <reajason1225@gmail.com>
2025-06-26 17:26:46 +0800

    feat: support Apusic V10

---
+++ generator/src/main/java/com/reajason/javaweb/memshell/ServerType.java
@@ -0,0 +1,30 @@
+package com.reajason.javaweb.memshell;
+
+/**
+ * @author ReaJason
+ * @since 2025/6/11
+ */
+public class ServerType {
+    public static final String TOMCAT = "Tomcat";
+    public static final String JETTY = "Jetty";
+    public static final String JBOSS_AS = "JBossAS";
+    public static final String JBOSS_EAP6 = "JBossEAP6";
+    public static final String UNDERTOW = "Undertow";
+    public static final String JBOSS_EAP7 = "JBossEAP7";
+    public static final String WILDFLY = "Wildfly";
+    public static final String RESIN = "Resin";
+    public static final String GLASSFISH = "Glassfish";
+    public static final String PAYARA = "Payara";
+    public static final String WEBLOGIC = "WebLogic";
+    public static final String WEBSPHERE = "WebSphere";
+    public static final String SPRING_WEBMVC = "SpringWebMVC";
+    public static final String SPRING_WEBFLUX = "SpringWebFlux";
+    public static final String XXL_JOB = "XXL-JOB";
+    public static final String APUSIC = "Apusic";
+    public static final String BES = "BES";
+    public static final String INFORSUITE = "InforSuite";
+    public static final String PRIMETON = "Primeton";
+    public static final String TONGWEB6 = "TongWeb6";
+    public static final String TONGWEB7 = "TongWeb7";
+    public static final String TONGWEB8 = "TongWeb8";
+}

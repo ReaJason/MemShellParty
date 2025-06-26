e0425613ca344fc4fcf243ed52b2c5fcae6f14fe
ReaJason <reajason1225@gmail.com>
2025-06-26 17:58:53 +0800

    chore: version 2.0.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
 }
-version = "1.10.0"
+version = "2.0.0-SNAPSHOT"
 
 tasks.register("publishAllToMavenCentral") {
     val isSnapshot = rootProject.version.toString().endsWith("-SNAPSHOT")

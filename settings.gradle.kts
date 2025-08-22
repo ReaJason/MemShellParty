4e505d6056e6411ca4c7ae584e69fa989a271bfb
ReaJason <reajason1225@gmail.com>
2025-08-22 18:41:48 +0800

    build: add more repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,9 @@ pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+        maven("https://www.jitpack.io")
+        maven("https://central.sonatype.com/repository/maven-snapshots")
     }
 
     includeBuild("build-logic")
@@ -15,6 +18,9 @@ dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         mavenCentral()
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+        maven("https://www.jitpack.io")
+        maven("https://central.sonatype.com/repository/maven-snapshots")
     }
 }
 

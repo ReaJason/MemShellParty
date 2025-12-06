c5cdb03a9f5d80b1c11a9d8bf31af5438219b5b4
ReaJason <reajason1225@gmail.com>
2025-12-06 14:30:21 +0800

    test: fix failed cases

---
+++ vul/vul-springboot2-undertow/src/main/java/com/reajason/javaweb/vul/springboot2/controller/BigIntegerClassLoaderController.java
@@ -0,0 +1,24 @@
+package com.reajason.javaweb.vul.springboot2.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ReaJason
+ * @since 2025/12/6
+ */
+@RestController
+@RequestMapping("/biginteger")
+public class BigIntegerClassLoaderController extends ClassLoader {
+    static byte[] decodeBigInteger(String bigIntegerStr) throws Exception {
+        Class<?> decoderClass = Class.forName("java.math.BigInteger");
+        return (byte[]) decoderClass.getMethod("toByteArray").invoke(decoderClass.getConstructor(String.class, int.class).newInstance(bigIntegerStr, Character.MAX_RADIX));
+    }
+
+    @PostMapping
+    public void base64ClassLoader(String data) throws Exception {
+        byte[] bytes = decodeBigInteger(data);
+        defineClass(null, bytes, 0, bytes.length).newInstance();
+    }
+}

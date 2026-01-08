7205b8dbb1a3fb41f4c35e79a6aea7cc719d4092
ReaJason <reajason1225@gmail.com>
2026-01-09 01:51:14 +0800

    feat: support addFilterFirst for GlassFish

--- integration-test/src/test/java/com/reajason/javaweb/integration/probe/DetectionTool.java
+++ integration-test/src/test/java/com/reajason/javaweb/integration/probe/DetectionTool.java
@@ -4,10 +4,7 @@ import com.reajason.javaweb.buddy.TargetJreVersionVisitorWrapper;
 import com.reajason.javaweb.probe.payload.BasicInfoPrinter;
 import com.reajason.javaweb.probe.payload.JdkProbe;
 import com.reajason.javaweb.probe.payload.ServerProbe;
-import com.reajason.javaweb.probe.payload.filter.JettyFilterProbe;
-import com.reajason.javaweb.probe.payload.filter.ResinFilterProbe;
-import com.reajason.javaweb.probe.payload.filter.TomcatFilterProbe;
-import com.reajason.javaweb.probe.payload.filter.UndertowFilterProbe;
+import com.reajason.javaweb.probe.payload.filter.*;
 import com.reajason.javaweb.utils.CommonUtil;
 import net.bytebuddy.ByteBuddy;
 import org.apache.commons.codec.binary.Base64;
@@ -52,5 +49,9 @@ public class DetectionTool {
 
     public static String getUndertowFilterProbe() {
         return getBase64Class(UndertowFilterProbe.class);
+    }
+
+    public static String getGlassFishFilterProbe() {
+        return getBase64Class(GlassFishFilterProbe.class);
     }
 }

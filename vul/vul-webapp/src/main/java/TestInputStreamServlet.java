b1e39f2bda493b4c9e8fc44a3517cbbb16b267bd
ReaJason <reajason1225@gmail.com>
2026-01-08 19:33:11 +0800

    test: add classes

---
+++ vul/vul-webapp/src/main/java/TestInputStreamServlet.java
@@ -0,0 +1,41 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author ReaJason
+ * @since 2025/12/31
+ */
+public class TestInputStreamServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String one = req.getParameter("one");
+        req.setCharacterEncoding("UTF-8");
+        String body = null;
+        InputStream inputStream = req.getInputStream();
+        StringBuilder bodyBuilder = new StringBuilder();
+        if (one != null) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            int b;
+            while ((b = inputStream.read()) != -1) {
+                outputStream.write(b);
+            }
+            bodyBuilder.append(outputStream.toString("UTF-8"));
+            outputStream.close();
+        } else {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                bodyBuilder.append(new String(buffer, 0, bytesRead, "UTF-8"));
+            }
+        }
+        body = bodyBuilder.toString();
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().println(body);
+    }
+}
---
+++ vul/vul-webapp/src/main/java/TestReaderServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * @author ReaJason
+ * @since 2025/12/31
+ */
+public class TestReaderServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String one = req.getParameter("one");
+        String body = null;
+        BufferedReader reader = req.getReader();
+        StringBuilder bodyBuilder = new StringBuilder();
+        if (one != null) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                bodyBuilder.append((char) c);
+            }
+        } else {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                bodyBuilder.append(line);
+            }
+        }
+        body = bodyBuilder.toString();
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().println(body);
+    }
+}

f170ca4514988b7fd4e620aab62f2d4ba40e6a5a
ReaJason <reajason1225@gmail.com>
2026-01-09 20:24:13 +0800

    refactor: use commonUtil for packageName

--- integration-test/src/test/java/com/reajason/javaweb/integration/probe/jbossas/Jboss423ContainerTest.java
+++ integration-test/src/test/java/com/reajason/javaweb/integration/probe/jbossas/Jboss423ContainerTest.java
@@ -7,6 +7,7 @@ import com.reajason.javaweb.integration.probe.DetectionTool;
 import com.reajason.javaweb.memshell.ShellTool;
 import com.reajason.javaweb.memshell.ShellType;
 import com.reajason.javaweb.packer.Packers;
+import com.reajason.javaweb.utils.CommonUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -96,6 +97,6 @@ public class Jboss423ContainerTest {
         String data = VulTool.post(url + "/b64", DetectionTool.getTomcatFilterProbe());
         List<String> filter = ProbeAssertion.getFiltersForContext(data, "/app");
         String filterName = ProbeAssertion.extractFilterName(filter.get(0));
-        assertThat(filterName, anyOf(startsWith("org.apache.http.web.handlers")));
+        assertThat(filterName, anyOf(startsWith(CommonUtil.getWebPackageNameForServer(Server.JBoss))));
     }
 }

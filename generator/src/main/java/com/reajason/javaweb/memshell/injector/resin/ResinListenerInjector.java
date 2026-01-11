c51b4e11dc7934f2fd5445cc104c36f0de2b86a2
ReaJason <reajason1225@gmail.com>
2026-01-12 01:23:14 +0800

    fix: context not found test not work

--- generator/src/main/java/com/reajason/javaweb/memshell/injector/resin/ResinListenerInjector.java
+++ generator/src/main/java/com/reajason/javaweb/memshell/injector/resin/ResinListenerInjector.java
@@ -38,7 +38,7 @@ public class ResinListenerInjector {
         } catch (Throwable throwable) {
             msg += "context error: " + getErrorMessage(throwable);
         }
-        if (contexts == null) {
+        if (contexts == null || contexts.isEmpty()) {
             msg += "context not found";
         } else {
             for (Object context : contexts) {

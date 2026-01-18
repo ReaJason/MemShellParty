a85e0a9282dc06e0a6d5c667e917f7771cc41af1
ReaJason <reajason1225@gmail.com>
2026-01-18 23:34:01 +0800

    test: add empty component

---
+++ vul/vul-webapp/src/main/java/EmptyFilter.java
@@ -0,0 +1,19 @@
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EmptyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

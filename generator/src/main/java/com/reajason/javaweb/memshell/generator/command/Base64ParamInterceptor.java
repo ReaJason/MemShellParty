666aed90c3ad5271fab8ae57a52024371412653b
ReaJason <reajason1225@gmail.com>
2026-04-26 13:16:14 +0800

    feat: support command base64 encryptor

---
+++ generator/src/main/java/com/reajason/javaweb/memshell/generator/command/Base64ParamInterceptor.java
@@ -0,0 +1,16 @@
+package com.reajason.javaweb.memshell.generator.command;
+
+import com.reajason.javaweb.utils.ShellCommonUtil;
+import net.bytebuddy.asm.Advice;
+
+/**
+ * @author ReaJason
+ * @since 2025/4/27
+ */
+public class Base64ParamInterceptor {
+
+    @Advice.OnMethodExit
+    public static void enter(@Advice.Argument(value = 0) String param, @Advice.Return(readOnly = false) String returnValue) throws Exception {
+        returnValue = ShellCommonUtil.base64DecodeToString(param);
+    }
+}

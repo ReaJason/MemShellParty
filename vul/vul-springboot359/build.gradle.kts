0a9a68ee7f944bb766061bd90611d54652f969ec
ReaJason <reajason1225@gmail.com>
2026-05-24 21:17:15 +0800

    fix: SpELGzipPacker class not found

---
+++ vul/vul-springboot359/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("war")
+    id("org.springframework.boot") version "3.5.9"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("com.h2database:h2")
+    implementation("ognl:ognl:2.7.3")
+    implementation("commons-jxpath:commons-jxpath:1.3")
+    implementation("commons-beanutils:commons-beanutils:1.9.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

